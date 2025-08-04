5893f6d9d8c0b9e1407d0a8c0bc73722d089d492
Sambeau <sambeauprak@gmail.com>
2025-08-04 09:59:53 +0200

    :tada: init

---
+++ intro.php
@@ -0,0 +1,32 @@
+<?php
+
+$a = "Sambeau";
+$b = 2;
+
+$c = $a . " " . $b;
+
+// echo $c;
+
+/*
+$nom = readline("Saisir votre nom : ");
+
+if ($nom == "Sambeau") { // alors
+    echo "Je n'aime pas Sambeau";
+} elseif ($nom == "Sambelle") {
+    echo "Arrête de faire semblant Sambeau, je t'ai vu.";
+} else {
+    echo "Bonjour, je m'appelle " . $nom;
+} // fin si
+*/
+
+$table2 = ["Céline", "Imen", "Chahinez", "Syrine", "Dylan", "M-A", "Mina"];
+
+print_r($table2);
+
+/*
+echo "Voici la liste des étudiants de la table 2 : \n";
+
+for ($i = 0; $i < count($table2); $i = $i + 1 ) {
+    echo "- " . $table2[$i] . "\n";
+}
+*/
